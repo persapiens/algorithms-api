e2fa24dc66997ad2bc67ba7c5ac6f45ab5e4243c
Marcelo Romulo Fernandes <persapiens@gmail.com>
2017-05-23 09:08:33 -0300

    mais alguns testes da arvore rubro negra

--- src/main/java/org/persapiens/algorithms/hash/QuadraticProbing.java
+++ src/main/java/org/persapiens/algorithms/hash/QuadraticProbing.java
@@ -3,7 +3,6 @@ package org.persapiens.algorithms.hash;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 /**
  *
--- src/test/java/org/persapiens/algorithms/tree/IntegerRedBlackTreeTest.java
+++ src/test/java/org/persapiens/algorithms/tree/IntegerRedBlackTreeTest.java
@@ -182,5 +182,74 @@ public class IntegerRedBlackTreeTest extends AbstractBinaryTreeTest<IntegerRedBl
 		checkTreeNode(node14, node19, node12, node17, BLACK);		
 		checkTreeNode(node20, node19, tree.getNill(), node22, BLACK);		
 	}
+	
+	public void example133CormenPage314Delete18e19e20() {
+		IntegerRedBlackTree tree = example133CormenPage314();
+		
+		IntegerRedBlackTreeNode node17 = tree.search(17);
+		IntegerRedBlackTreeNode node22 = tree.search(22);
+		IntegerRedBlackTreeNode node12= tree.search(12);		
+		IntegerRedBlackTreeNode node7 = tree.search(7);
+		IntegerRedBlackTreeNode node11= tree.search(11);		
+		IntegerRedBlackTreeNode node9 = tree.search(9);
+		IntegerRedBlackTreeNode node14 = tree.search(14);
 
+		tree.delete(tree.search(18));
+		tree.delete(tree.search(19));
+		tree.delete(tree.search(20));
+		
+		checkTreeNode(node11, node7, node9, node14, BLACK);
+		checkTreeNode(node9, node11, tree.getNill(), tree.getNill(), BLACK);		
+		checkTreeNode(node14, node11, node12, node22, RED);		
+		checkTreeNode(node22, node14, node17, tree.getNill(), BLACK);		
+		checkTreeNode(node17, node22, tree.getNill(), tree.getNill(), RED);		
+	}
+	
+	private IntegerRedBlackTree example134CormenPage317() {
+		return createTree(true, 11, 2, 14, 1, 7, 15, 5, 8);
+	}
+	
+	public void example134CormenPage317CaseA() {
+		IntegerRedBlackTree tree = example134CormenPage317();
+		
+		IntegerRedBlackTreeNode node4 = tree.insert(4);
+		
+		IntegerRedBlackTreeNode node5 = tree.search(5);
+		IntegerRedBlackTreeNode node2 = tree.search(2);
+		IntegerRedBlackTreeNode node7 = tree.search(7);		
+		IntegerRedBlackTreeNode node11= tree.search(11);		
+		IntegerRedBlackTreeNode node1 = tree.search(1);
+		IntegerRedBlackTreeNode node8 = tree.search(8);		
+		IntegerRedBlackTreeNode node14 = tree.search(14);		
+		
+		checkTreeNode(node7, tree.getNill(), node2, node11, BLACK);
+		checkTreeNode(node2, node7, node1, node5, RED);		
+		checkTreeNode(node11, node7, node8, node14, RED);		
+		checkTreeNode(node5, node2, node4, tree.getNill(), BLACK);		
+		checkTreeNode(node4, node5, tree.getNill(), tree.getNill(), RED);
+	}
+	
+	private IntegerRedBlackTree example134CormenPage317Invertido() {
+		return createTree(true, 11, 2, 20, 1, 15, 25, 14, 16);
+	}
+	
+	public void example134CormenPage317CaseAInvertido() {
+		IntegerRedBlackTree tree = example134CormenPage317Invertido();
+		
+		IntegerRedBlackTreeNode node17 = tree.insert(17);
+		
+		IntegerRedBlackTreeNode node15 = tree.search(15);
+		IntegerRedBlackTreeNode node2 = tree.search(2);
+		IntegerRedBlackTreeNode node20 = tree.search(20);
+		IntegerRedBlackTreeNode node11= tree.search(11);		
+		IntegerRedBlackTreeNode node16 = tree.search(16);
+		IntegerRedBlackTreeNode node25 = tree.search(25);		
+		IntegerRedBlackTreeNode node14 = tree.search(14);		
+		
+		checkTreeNode(node15, tree.getNill(), node11, node20, BLACK);		
+		checkTreeNode(node20, node15, node16, node25, RED);		
+		checkTreeNode(node16, node20, tree.getNill(), node17, BLACK);		
+		checkTreeNode(node17, node16, tree.getNill(), tree.getNill(), RED);
+		checkTreeNode(node11, node15, node2, node14, RED);
+	}
 }
